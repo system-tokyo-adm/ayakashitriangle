94f6dd0434ecb972160010b8f78d54017cc36f72
keita <keita@keitanoMacBook-Pro.local>
2023-01-05 18:21:20 +0900

    first commit

--- build/app/generated/source/buildConfig/debug/com/example/quiz_spreadsheet/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/example/quiz_spreadsheet/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.quiz_spreadsheet;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "";
+  public static final String APPLICATION_ID = "com.gmail.seriousapp777_kubosan";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 2;
   public static final String VERSION_NAME = "1.0.1";
--- lib/const.dart
+++ lib/const.dart
@@ -5,14 +5,14 @@ const bool kAdDisplay = true;
 // 設定
 const String kApiKey = "AIzaSyDh8cegEZHAXsIokjsALZicQmvOSO8ZjF0";
 const String kSheetId = "1cwZb46fzaZimQZEm2VQq_Xv7TI20LPIrokpVK_85QDg";
-const String kSheetQuiz = "あやかしトライアングル";
+const String kSheetQuiz = "久保さんは僕を許さない";
 const String kSheetResult = "結果";
-const String kSheetShindan = "あやかしトライアングル-診断";
-const MaterialColor kPrimaryColorSwatch = Colors.blue;
-const Color kPrimaryColor = Color(0xFF64B5F6);
+const String kSheetShindan = "久保さんは僕を許さない-診断";
+const MaterialColor kPrimaryColorSwatch = Colors.green;
+const Color kPrimaryColor = Color(0xFFF66492);
 const Color kBackgroundColor = Color(0x66000000);
-const String kAppName = "あやかしトライアングルクイズアプリ";
-const String kAppUrl = "https://play.google.com/store/apps/details?id=com.gmail.wonderapp777.quizhanayome";
+const String kAppName = "久保さんは僕を許さないクイズアプリ";
+const String kAppUrl = "https://play.google.com/store/apps/details?id=com.gmail.seriousapp777.quizkubosan";
 // クイズのモード
 const bool kIsQuizRandom = true;
 const bool kIsChoiceRandom = true;
@@ -22,7 +22,7 @@ const bool kIsShindanReward = true;
 const int kClearPercent = 80;
 // 広告
 const String kAdmobIdBanner = "";
-const String kAdmobIdBannerRectangle = "ca-app-pub-8929796513816949/7444984051";
-const String kAdmobIdInterstitialPlay = "ca-app-pub-8929796513816949/2192657378";
-const String kAdmobIdInterstitialResult = "ca-app-pub-8929796513816949/5940330699";
+const String kAdmobIdBannerRectangle = "ca-app-pub-8929796513816949/4178441246";
+const String kAdmobIdInterstitialPlay = "ca-app-pub-8929796513816949/6257809673";
+const String kAdmobIdInterstitialResult = "ca-app-pub-8929796513816949/4944728001";
 const String kAdmobIdReward = "";
